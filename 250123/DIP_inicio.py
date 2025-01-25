470f3000a66db9c5af98d25e15b85af56cac7f60
karen <karenarangov@gmail.com>
2025-01-25 17:53:06 -0500

    Agregue el calculo de contraste

--- 250123/DIP_inicio.py
+++ 250123/DIP_inicio.py
@@ -5,33 +5,37 @@ import sklearn
 import jupyter
 import cv2
 
-# Mostrar imagen
+# Show image
 image = cv2.imread(r'C:\Users\karen\OneDrive\Escritorio\DIP\250123\brain.png',cv2.IMREAD_GRAYSCALE)
 cv2.imshow('Imagen', image)
 
-# Tamaño
+# Size
 print(image.shape)
 
-# Resolución
+# Resolution
 h, w = image.shape
 print('Resolucion:', w, 'x', h)
 
-# Histograma para cada canal
+# Histogram
 hist = cv2.calcHist([image], [0], None, [256], [0, 256])
 plt.bar(range(256), hist[:, 0], width=1, color='gray', alpha=0.7)
-plt.title('Histograma')
-plt.xlabel('Valores de intensidad')
-plt.ylabel('Número de píxeles')
+plt.title('Histogram')
+plt.xlabel('Intensity')
+plt.ylabel('Pixels')
 plt.show()
 
-# Brillo (promedio de intensidad)
+# Bright 
 bright = np.mean(image)
-print('Brillo promedio:', bright)
+print('Bright:', bright)
 
-# Cuantización
+# Quantization
 unique_levels = np.unique(image) # Obtener los valores únicos de la imagen
 num_levels = len(unique_levels) # Contar los niveles únicos
-print('Cuantizacion:', num_levels)
+print('Quantization:', num_levels)
+
+# Contrast 
+contrast = image.std()
+print('Contrast:', contrast)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()  # Cierra todas las ventanas abiertas por OpenCV
