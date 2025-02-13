1160d8dc7d0b734bf4d170f0ce584fbcbfee4f42
karen <karenarangov@gmail.com>
2025-02-13 12:01:52 -0500

    clase guis and poo

---
+++ 250213/250213.py
@@ -0,0 +1,21 @@
+#METODOS:
+#constructor: funcion que se llama cuando se inicializa la clase
+#Abstraccion, herencia, polimorfismo, encapsulamiento
+
+class Persona:
+    def __init__(self, nombre,apellido, edad): #constructor, self hace referencia a la instancia actual
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+    def saludar(self): #self es el primer paramtro de toda funcion
+        print(f"Hola, mi nombre es {self.nombre} {self.apellido} y tengo {self.edad} años.")
+
+#Crear una instancia
+persona1 = Persona('Mariana','Nieto', 21)
+persona2 = Persona('Majo','Rodriguez', 21)
+persona1.saludar()
+persona2.saludar()
+
+#INTERFACES GRAFICAS: kivy o cualquier otro framework
+#
---
+++ 250213/Mykivy.py
@@ -0,0 +1,42 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+import cv2
+
+class miVentana(BoxLayout): #herencia, definir mi ventana a partir de la libreria
+    def __init__(self,**kwargs): #CONSTRUCTOR DE LA CLASE
+        super().__init__(**kwargs) #CONSTRUCTOR ORIGINAL
+        self.cajaInterna = BoxLayout(orientation = 'horizontal')
+        self.orientation = 'vertical'
+        self.L = Label(text = 'Hi', color = [0.2,0.2,1,1], bold = True, font_size = 50, size_hint=(0.7,0.3))
+        self.cargar = Button(text = 'Cargar imagen', size_hint = (0.5,0.3), pos_hint = {'center_x':0.5, 'center_y':0.5})
+        self.add_widget(self.cargar)
+
+        self.cargar.bind(on_press = self.cargar_imagen) #vincular accion sobre boton
+        self.cajaInterna.add_widget(self.L)
+        self.image = Image(size_hint=(0.7,1))
+        self.cajaInterna.add_widget(self.image)
+        self.add_widget(self.cajaInterna)
+        
+
+    def cargar_imagen(self,instance):
+        self.L.text = 'Auch!'
+        #self.image.source = (r"C:\Users\karen\OneDrive\Escritorio\DIP\250206\1-037.JPG")
+        imagenCV = cv2.imread(r"C:\Users\karen\OneDrive\Escritorio\DIP\250206\1-037.JPG")
+        buffer = cv2.flip(imagenCV, 0).tostring() #convierte imagen a vector tipo texto
+        textura = Texture.create(size = (imagenCV.shape[1], imagenCV.shape[0]), colorfmt = 'bgr')
+        textura.blit_buffer(buffer, colorfmt = 'bgr', bufferfmt = 'ubyte')
+        self.image.texture = textura
+
+        
+class myApp(App):
+    def build(self):
+        return miVentana()
+    
+if __name__ == '__main__':
+    myApp().run()
+
+#Los layouts nos ayudan a organizar las funciones en nuestra interfaz
